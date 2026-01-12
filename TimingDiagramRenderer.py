6f9502385bf11cfd74e2d30479f37c6fb2ed6c78
Julián Calderón Almendros <earendil.elrond.arwen@gmail.com>
2026-01-12 19:32:45 +0100

    commit

--- TimingDiagramRenderer.py
+++ TimingDiagramRenderer.py
@@ -2,31 +2,25 @@ from ExamDataModel import Exercise5Data
 
 class TimingDiagramRenderer:
     def render(self, data: Exercise5Data) -> str:
-        # Usamos resizebox para asegurar que el cronograma se ajuste al ancho de la página
-        # independientemente de la cantidad de ciclos.
-        
-        # Ajustes de estilo:
-        # y=1.5cm: Aumenta la altura de las señales
-        # timing/slope=0: Flancos verticales perfectos
-        # Eliminado 'row dist' que causaba error.
-        # Usamos \\[-1ex] para reducir el espacio entre filas manualmente.
+        # Usamos resizebox para asegurar que el cronograma se ajuste al ancho de la página.
+        # y=0.6cm: Reduce drásticamente la separación entre filas.
         
         latex = r"\begin{center}" + "\n"
         latex += r"\resizebox{\textwidth}{!}{%" + "\n"
-        latex += r"\begin{tikztimingtable}[timing/slope=0, x=2.0cm, y=1.5cm]" + "\n"
+        latex += r"\begin{tikztimingtable}[timing/slope=0, x=2.0cm, y=0.6cm]" + "\n"
         
         # Clock
-        latex += fr"CLK\hspace{{1em}} & {data.clk_sequence} \\[-1ex]" + "\n"
+        latex += fr"CLK\hspace{{1em}} & {data.clk_sequence} \\" + "\n"
         
         # Async signal (si existe)
         if data.has_async:
-            latex += fr"{data.async_type}(asyn)\hspace{{1em}} & {data.async_sequence} \\[-1ex]" + "\n"
+            latex += fr"{data.async_type}(asyn)\hspace{{1em}} & {data.async_sequence} \\" + "\n"
             
         # Input signal
-        latex += fr"E\hspace{{2.5em}} & {data.input_sequence} \\[-1ex]" + "\n"
+        latex += fr"E\hspace{{2.5em}} & {data.input_sequence} \\" + "\n"
         
         # Output placeholders
-        latex += fr"Q0 & [draw=none, fill=none] {data.output_placeholder} \\[-1ex]" + "\n"
+        latex += fr"Q0 & [draw=none, fill=none] {data.output_placeholder} \\" + "\n"
         latex += fr"Q1 & [draw=none, fill=none] {data.output_placeholder} \\" + "\n"
         
         latex += r"\extracode \tablegrid \end{tikztimingtable}%" + "\n"
